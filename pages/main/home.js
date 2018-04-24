9fcf28b758c784e76b715e2e93cfb84b7c19dcbd
yangbo05527 <yangbo@suneee.com>
2018-04-24 16:09:08 +0800

    商城首页和详情前端页面

---
+++ pages/main/home.js
@@ -0,0 +1,88 @@
+// pages/main/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    motto: 'MiHome_Store',
+    userInfo: {},
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 300,
+    height:280,
+    gooddetails: [
+      {
+        "banner": [
+          {
+            "pic_url": "http://static.home.mi.com/app/shop/img?id=shop_c2cf209c66a22818c7f5c269f6bbff12.jpeg&w=420&h=240&crop=a_90_0_240_240",
+          },
+          {
+            "pic_url": "http://static.home.mi.com/app/shop/img?id=shop_8dec2f08e5dd9d08b440f77a36e39e16.png&w=420&h=240&crop=a_90_0_240_240",
+          },
+          {
+            "pic_url": "http://static.home.mi.com/app/shop/img?id=shop_35a026ff12d476496f91d121911af0ce.jpg&crop=a_90_0_240_240",
+          }
+        ]
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
